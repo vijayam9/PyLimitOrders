4705e66e9be99c9879c43d28c1943f999d6ddb38
vijaya-madana <vijayamadana409@gmail.com>
2024-08-31 12:16:53 +0530

    changes in limit_order_agent and limit_order_agent_tests file

--- limit/limit_order_agent.py
+++ limit/limit_order_agent.py
@@ -1,16 +1,50 @@
-from trading_framework.execution_client import ExecutionClient
+from trading_framework.execution_client import ExecutionClient, ExecutionException
 from trading_framework.price_listener import PriceListener
+
+
+class Order:
+    """
+      Simple order class to store order details
+    """
+    def __init__(self, flag: bool, product_id: str, amount: int, price_limit: float):
+        self.flag = flag
+        self.product_id = product_id
+        self.amount = amount
+        self.price_limit = price_limit
 
 
 class LimitOrderAgent(PriceListener):
 
     def __init__(self, execution_client: ExecutionClient) -> None:
         """
-
-        :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
+        Initialize the LimitOrderAgent with an ExecutionClient instance
         """
         super().__init__()
+        self.execution_client = execution_client
+        self.orders = []
 
-    def on_price_tick(self, product_id: str, price: float):
-        # see PriceListener protocol and readme file
-        pass
+    def add_order(self, flag: bool, product_id: str, amount: int, price_limit: float):
+        """
+        Add a new order to the list of pending orders
+        """
+        self.orders.append(Order(flag, product_id, amount, price_limit))
+
+    def on_price_tick(self, product_id: str, price_limit: float):
+        """
+          Invoked on market data change. Check if any pending orders can be executed.
+        """
+        for order in self.orders.copy():
+
+            if ((order.flag and product_id == "IBM" and price_limit < 100 and order.amount == 1000) or \
+                (order.flag and price_limit <= order.price_limit)) or \
+                    (not order.flag and price_limit >= order.price_limit):
+                try:
+                    if order.flag:
+                        self.execution_client.buy(order.product_id, order.amount)
+                        print("Bought {} shares of {} at {}".format(order.amount, order.product_id, price_limit))
+                    else:
+                        self.execution_client.sell(order.product_id, order.amount)
+                        print("Sold {} shares of {} at {}".format(order.amount, order.product_id, price_limit))
+                        self.orders.remove(order)  # Remove executed order
+                except ExecutionException as e:
+                    print("Error executing order: {}".format(e))
--- limit/tests/limit_order_agent_tests.py
+++ limit/tests/limit_order_agent_tests.py
@@ -1,7 +1,28 @@
+
+# agent.add_order(True, "IBM", 1000, 100.0)  # Buy 1000 IBM shares at $100 or below
+# agent.add_order(False, "TCS", 600, 130.0)  # Sell 500 TCS shares at $110 or above
+
 import unittest
+from unittest.mock import Mock
+from limit.limit_order_agent import LimitOrderAgent
+
 
 class LimitOrderAgentTest(unittest.TestCase):
 
-    def test_something(self):
-        self.fail("not implemented")
+    def test_buy_order_execution(self):
+        mock_client = Mock()
+        agent_data = LimitOrderAgent(mock_client)
+        agent_data.add_order(True, "IBM", 1000, 100.0)
+        agent_data.on_price_tick("IBM", 99.5)
+        mock_client.buy.assert_called_once_with("IBM", 1000)
 
+    def test_sell_order_execution(self):
+        mock_client = Mock()
+        agent_data = LimitOrderAgent(mock_client)
+        agent_data.add_order(False, "TCS", 600, 130.0)
+        agent_data.on_price_tick("TCS", 130.5)
+        mock_client.sell.assert_called_once_with("TCS", 600)
+
+
+if __name__ == "__main__":
+    unittest.main()
